daae7d9b6f426b9be2839b26b0889884b9d63118
bung_anb <bunganabila1005@gmail.com>
2025-07-25 15:19:44 +0700

    feat : add bgm

---
+++ Assets/Scripts/BGMManager.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class BGMManager : MonoBehaviour
+{
+    public static BGMManager Instance;
+    private AudioSource audioSource;
+
+    void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+        audioSource = GetComponent<AudioSource>();
+        if (audioSource != null && !audioSource.isPlaying)
+        {
+            audioSource.loop = true;
+            audioSource.Play();
+        }
+    }
+}
