c0a79182ecbadd12e2b1f2720f04dbfe8b9c3d6f
bung_anb <bunganabila1005@gmail.com>
2025-07-25 14:37:26 +0700

    feat : add sprite weapon changer

--- Assets/Scripts/WeaponData.cs
+++ Assets/Scripts/WeaponData.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 public class WeaponData : ScriptableObject
 {
     public string weaponName;
+    public Sprite weaponSprite;
     public GameObject bulletPrefab;
     public bool hasThisWeapon;
 }
